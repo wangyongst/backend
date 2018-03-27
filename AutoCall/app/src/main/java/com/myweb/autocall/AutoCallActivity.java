ff62788fdb2ff133f1e5110e5d2a4207ca0b7b30
wangyongst <wangyongst@gmail.com>
2018-03-27 17:34:33 +0800

    office

--- AutoCall/app/src/main/java/com/myweb/autocall/AutoCallActivity.java
+++ AutoCall/app/src/main/java/com/myweb/autocall/AutoCallActivity.java
@@ -1,12 +1,9 @@
 package com.myweb.autocall;
 
-import android.annotation.SuppressLint;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.*;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -15,10 +12,29 @@ import android.view.View;
 public class AutoCallActivity extends AppCompatActivity {
 
     private View mContentView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auto_call);
         mContentView = findViewById(R.id.start);
+
+        final Button startButton = (Button) findViewById(R.id.start_button);
+        final TextView startInfo = (TextView) findViewById(R.id.start_info);
+        final EditText time = (EditText) findViewById(R.id.time);
+        final CheckBox sendMessage = (CheckBox) findViewById(R.id.send_message);
+        final EditText message = (EditText) findViewById(R.id.message);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (startButton.getText().equals("开始")) {
+                    startInfo.setText("程序启动，开始自动拨号！"+ sendMessage.isChecked() + message.getText());
+                    startButton.setText("停止");
+                } else {
+                    startInfo.setText("程序停止，停止自动拨号！" + time.getText());
+                    startButton.setText("开始");
+                }
+            }
+        });
     }
 }
